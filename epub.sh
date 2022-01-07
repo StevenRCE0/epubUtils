20ffbb12f767c11d31d4d1a9251eefcdc7b16422
StevenRCE0 <40051361+StevenRCE0@users.noreply.github.com>
2022-01-08 02:03:43 +0800

    initial commit

---
+++ epub.sh
@@ -0,0 +1,27 @@
+toolboxPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+[[ $readerPath ]] || {
+    echo "Set a environmental variable \"readerPath\" to continue..."
+    exit
+}
+cd "$readerPath"
+
+processPDF() {
+    echo "Processing book $2..."
+    docker run -ti --rm -v `pwd`:/temp dodeeric/pdf2epubex pdf2epubEX $1
+    [[ -e 'mybook.pdf' ]] || rm mybook.pdf
+    for outputFile in *.epub
+    do
+        [[ -f "$outputFile" ]] || continue
+        mv $outputFile 'toMerge.epub'
+    done
+    $toolboxPath/docpub -f epub -o . "$1" --fname 'index.epub'
+    python3 $toolboxPath/merge.py 'toMerge.epub' 'index.epub' "$2"
+    mv $1 '../PDF Archives'
+    mv "$2.epub" '..'
+}
+
+for file in *.pdf
+do
+    [[ -f "$file" ]] || continue
+    processPDF $file "${file%.pdf}"
+done
---
+++ merge.py
@@ -0,0 +1,51 @@
+import os
+import zipfile
+import sys
+import shutil
+
+# Usage: python3 merge.py <epub to merge into> <epub with proper nav> <output filename(without extension)>
+
+customCSS = '''
+.d.m1 {
+    border: none !important;
+    border-bottom: solid 1px #000 !important;
+}
+'''
+
+assert isinstance(sys.argv[3], str)
+workingDirectory = str(os.getcwd())
+
+files = {
+    'toMerge': os.path.join(workingDirectory, sys.argv[1]),
+    'index': os.path.join(workingDirectory, sys.argv[2])
+}
+
+for fileType, fileName in files.items():
+    fileToUnzip = zipfile.ZipFile(fileName)
+    fileToUnzip.extractall(fileType)
+    os.remove(fileName)
+
+nav = open(os.path.join(workingDirectory, 'index', 'OEBPS', 'nav.xhtml'), 'r')
+toMergeNavFile = open(os.path.join(workingDirectory, 'toMerge', 'OEBPS', 'nav.xhtml'), 'w')
+styleFile = open(os.path.join(workingDirectory, 'toMerge', 'OEBPS', 'mybook.css'), 'a+')
+
+toMergeNavFile.write(nav.read().replace('href=\"page', 'href=\"mybook0'))
+styleFile.write(customCSS)
+nav.close()
+toMergeNavFile.close()
+styleFile.close()
+
+def zip_dir(path):
+    zf = zipfile.ZipFile(sys.argv[3] + '.epub'.format(path), 'w', zipfile.ZIP_DEFLATED)
+   
+    for root, dirs, files in os.walk(path):
+        for file_name in files:
+            if file_name == '.DS_Store':
+                continue
+            relativePath = str(root).replace(path, '')
+            zf.write(os.path.join(root, file_name), os.path.join(relativePath, file_name))
+
+zip_dir(os.path.join(workingDirectory, 'toMerge'))
+
+for folder in files.keys():
+    shutil.rmtree(os.path.join(workingDirectory, folder))
